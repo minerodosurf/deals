d9a9776c0ef44308cf04263dd4ac39da7e31e7c1
Marcelo Moreira <minerodosurf@gmail.com>
2016-05-18 15:12:13 +1000

    Create deals-version2.js

---
+++ deals-version2.js
@@ -0,0 +1,30 @@
+
+(function ($, root, undefined) {
+  $(document).ready(function(){
+    $('.deals-gallery .owl-carousel').owlCarousel({
+                                                   loop:true,
+                                                   margin:15,
+                                                   nav:true,
+                                                   autoHeight:true, lazyLoad:true,
+                                                   responsive:{
+                                                     0:{
+                                                       items:1
+                                                     },
+                                                     580:{
+                                                       items:1
+                                                     },
+                                                     768:{
+                                                       items:2
+                                                     },
+                                                     1024:{
+                                                       items:3,
+                                                       margin:10
+                                                     }
+                                                   },
+                                                   navText: [
+                                                     '<i class="fa fa-angle-left"></i>',
+                                                     '<i class="fa fa-angle-right"></i>'
+                                                   ]
+                                                 });
+  });
+})(jQuery, this);
